00477a8fdda07a7328adc9525d1c001373b2cc10
isaacrdz <zack_cpp@hotmail.com>
2017-10-19 20:14:27 -0500

    loginvc ui added

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,22 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Isaac Rodriguez on 10/19/17.
+//  Copyright © 2017 Isaac Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Isaac Rodriguez on 10/19/17.
+//  Copyright © 2017 Isaac Rodriguez. All rights reserved.
+//
+
+import Foundation
+
+let TO_LOGIN = "toLogin"
+
+
