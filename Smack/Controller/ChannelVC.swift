554e544d548356d9d1088bf30ea96762e96b7c6a
isaacrdz <zack_cpp@hotmail.com>
2017-11-17 22:04:34 -0600

    loginVCController

--- Smack/Controller/ChannelVC.swift
+++ Smack/Controller/ChannelVC.swift
@@ -14,15 +14,29 @@ class ChannelVC: UIViewController {
     
     
     @IBOutlet weak var loginBtn: UIButton!
+    @IBOutlet weak var userImage: CircleImage!
     @IBAction func prepareForUnwind(Segue:UIStoryboardSegue){}
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
+        NotificationCenter.default.addObserver(self, selector: #selector(ChannelVC.userDataDidChange(_:)), name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
     }
 
 
     @IBAction func loginBtnPressed(_ sender: Any) {
         performSegue(withIdentifier: TO_LOGIN, sender: nil)
+    }
+    
+    @objc func userDataDidChange(_ notif: Notification){
+        if AuthService.instance.isLoggedIn{
+            loginBtn.setTitle(UserDataService.instance.name, for: .normal)
+            userImage.image = UIImage(named: UserDataService.instance.avatarName)
+        } else {
+            loginBtn.setTitle("Login", for: .normal)
+            userImage.image = UIImage(named:"menuProfileIcon")
+            userImage.backgroundColor = UIColor.clear
+            
+        }
     }
 }
